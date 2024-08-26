2e0219e00a1e054523bc5d2370be8b58910bc2df
Ojjoj <karamkassem133@gmail.com>
2024-08-26 21:59:58 +0300

    levenshtein dependancy removed

--- helper_actions.py
+++ helper_actions.py
@@ -1,7 +1,6 @@
 import requests
 from random import randint
 import os
-import Levenshtein
 from typing import List
 from rich import print
 
@@ -71,11 +70,31 @@ def search_algorithm(names: List[str], query: str, max_distance: int = 2) -> Lis
     results.extend(prefix_matches)
 
     for name in names:
-        if Levenshtein.distance(name, query) <= max_distance:
+        if levenshtein(name, query) <= max_distance:
             if name not in results:
                 results.append(name)
 
     return list(set(results))
+
+
+def levenshtein(s1: str, s2: str) -> int:
+    m, n = len(s1), len(s2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        dp[i][0] = i
+    for j in range(n + 1):
+        dp[0][j] = j
+
+    # Fill the dp table
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+
+    return dp[m][n]
 
 
 def get_input(prompt_string: str = "Enter a username"):
